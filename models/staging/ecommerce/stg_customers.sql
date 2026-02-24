a4968218c3dc1c41fe03e9913f6ca91f0b0feb3d
adhamerlangga <adhamsiwi.as@gmail.com>
2026-02-24 21:41:17 +0700

    fix: change capitalize function to match duckdb limitation

--- models/staging/ecommerce/stg_customers.sql
+++ models/staging/ecommerce/stg_customers.sql
@@ -9,6 +9,6 @@ select
      trim(customer_id) as customer_id,
      trim(customer_unique_id) as customer_unique_id,
      lpad(cast(customer_zip_code_prefix as varchar), 5, '0') as customer_zip_code_prefix,
-     initcap(customer_city) as customer_city,
+     upper(left(customer_city, 1)) || lower(substring(customer_city, 2)) as customer_city,
      trim(customer_state) as customer_state
 from source
