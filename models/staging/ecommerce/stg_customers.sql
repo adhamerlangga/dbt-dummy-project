4fec76d10764fb422750ee5a28715c1d9b738656
adhamerlangga <adhamsiwi.as@gmail.com>
2026-02-24 21:37:49 +0700

    feat: staging customers

--- models/staging/ecommerce/stg_customers.sql
+++ models/staging/ecommerce/stg_customers.sql
@@ -6,6 +6,9 @@ with source as (
 )
 
 select 
-    *
+     trim(customer_id) as customer_id,
+     trim(customer_unique_id) as customer_unique_id,
+     lpad(cast(customer_zip_code_prefix as varchar), 5, '0') as customer_zip_code_prefix,
+     initcap(customer_city) as customer_city,
+     trim(customer_state) as customer_state
 from source
-limit 5
