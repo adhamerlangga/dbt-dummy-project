360db32e3e76424dc0b4b49f19af06086d7261e0
adhamerlangga <adhamsiwi.as@gmail.com>
2026-02-24 21:43:10 +0700

    fix: last column name

--- models/staging/ecommerce/stg_customers.sql
+++ models/staging/ecommerce/stg_customers.sql
@@ -10,5 +10,5 @@ select
      nullif(trim(customer_unique_id), '') as customer_unique_id,
      lpad(cast(customer_zip_code_prefix as varchar), 5, '0') as customer_zip_code_prefix,
      upper(left(customer_city, 1)) || lower(substring(customer_city, 2)) as customer_city,
-     nullif(trim(customer_state), '') 
+     nullif(trim(customer_state), '') as customer_state
 from source
