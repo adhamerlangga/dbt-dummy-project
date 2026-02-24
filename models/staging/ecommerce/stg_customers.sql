4133b9929b33d123220a84c361bd4b3452e10b2c
adhamerlangga <adhamsiwi.as@gmail.com>
2026-02-24 21:42:51 +0700

    feat: add null handling

--- models/staging/ecommerce/stg_customers.sql
+++ models/staging/ecommerce/stg_customers.sql
@@ -6,9 +6,9 @@ with source as (
 )
 
 select 
-     trim(customer_id) as customer_id,
-     trim(customer_unique_id) as customer_unique_id,
+     nullif(trim(customer_id), '') as customer_id,
+     nullif(trim(customer_unique_id), '') as customer_unique_id,
      lpad(cast(customer_zip_code_prefix as varchar), 5, '0') as customer_zip_code_prefix,
      upper(left(customer_city, 1)) || lower(substring(customer_city, 2)) as customer_city,
-     trim(customer_state) as customer_state
+     nullif(trim(customer_state), '') 
 from source
