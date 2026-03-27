00141a3da24fa3feaa02437b5b54871b9bd4c1da
adhamerlangga <adhamsiwi.as@gmail.com>
2026-03-27 18:39:43 +0700

    chore: reorder column for following convention

--- models/marts/fact_orders.sql
+++ models/marts/fact_orders.sql
@@ -38,20 +38,20 @@ select
     b.order_item_id,
     b.product_id,
     b.seller_id,
-    b.shipping_limit_date,
-    b.price,
-    b.freight_value,
-    
     o.customer_id,
+    
     o.order_status,
+    b.shipping_limit_date,
+    p.product_category_name,
+    
     o.order_purchased_at,
     o.order_approved_at,
     o.order_delivered_carrier_at,
     o.order_delivered_customer_at,
     o.order_estimated_delivery_at,
     
-    p.product_category_name,
-
+    b.price,
+    b.freight_value,
     b.price + b.freight_value as total_item_revenue
 from 
     base b
