08669835135135672f767303f82f8b13bd990684
adhamerlangga <adhamsiwi.as@gmail.com>
2026-03-28 10:25:32 +0700

    feat: create dim_customers model to select customer details

---
+++ models/marts/dim_customers.sql
@@ -0,0 +1,12 @@
+with customers as (
+    select
+        customer_id,
+        customer_unique_id,
+        customer_zip_code_prefix,
+        customer_city,
+        customer_state
+    from
+        {{ ref('stg_customers') }}
+)
+
+select * from customers
