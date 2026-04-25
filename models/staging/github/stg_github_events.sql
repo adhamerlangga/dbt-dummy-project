c496a6d83b8b93b017bfcf17d5bed7a501f798f9
adhamerlangga <adhamsiwi.as@gmail.com>
2026-04-25 13:26:57 +0700

    feat: utilize star dbt_utils

--- models/staging/github/stg_github_events.sql
+++ models/staging/github/stg_github_events.sql
@@ -1,19 +1,23 @@
-with github_events as (
+with source_events as (
     select
-        id as event_id,
-        type as event_type,
-        actor.id as actor_id,
-        actor.login as actor_login,
-        repo.id as repo_id,
-        repo.name as repo_name,
-        repo.url as repo_url,
-        payload,
-        public as is_public,
-        created_at,
-        org.id as org_id,
-        org.login as org_login,
-        org.url as org_url
-    from {{ source('github_archive_raw', 'github_events') }}
+        raw_events.id as event_id,
+        raw_events.type as event_type,
+        {{ dbt_utils.star(
+            from=source('github_archive_raw', 'github_events'),
+            except=['id', 'type', 'actor', 'repo', 'public', 'created_at', 'org'],
+            relation_alias='raw_events'
+        ) }},
+        raw_events.actor.id as actor_id,
+        raw_events.actor.login as actor_login,
+        raw_events.repo.id as repo_id,
+        raw_events.repo.name as repo_name,
+        raw_events.repo.url as repo_url,
+        raw_events.public as is_public,
+        raw_events.created_at,
+        raw_events.org.id as org_id,
+        raw_events.org.login as org_login,
+        raw_events.org.url as org_url
+    from {{ source('github_archive_raw', 'github_events') }} as raw_events
 )
 
 select
@@ -30,4 +34,4 @@ select
     org_id,
     org_login,
     org_url
-from github_events
+from source_events
